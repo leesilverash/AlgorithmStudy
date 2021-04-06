c2fff4504608a560e86a533df55224031d0c328a
leesilverash <leesilverash@gmail.com>
2021-04-06 23:51:03 +0900

    python study

---
+++ BOJ/BOJ_15596.py
@@ -0,0 +1,5 @@
+def solve(num_list):
+    result = 0
+    for i in range(len(num_list)):
+        result += num_list[i]
+    return result
---
+++ BOJ/BOJ_4673.py
@@ -0,0 +1,12 @@
+natural_num = set(range(1,10001))
+generated_num = set()
+for i in range(1,10001):
+    for j in str(i):
+        i += int(j)
+        generated_num.add(i)
+
+
+self_num = natural_num - generated_num
+
+print(self_num)
+
