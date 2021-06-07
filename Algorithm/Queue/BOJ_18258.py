fba5bacb6fc4977eebb42e583fec7f5c8138082b
leesilverash <leesilverash@gmail.com>
2021-06-08 00:31:39 +0900

    Queue-BOJ_18258.py

---
+++ Algorithm/Queue/BOJ_18258.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline().strip())
+que = deque()
+while(n):
+    cmd = sys.stdin.readline().strip().split()
+    if cmd[0] == 'push':
+        que.append(int(cmd[1]))
+    elif cmd[0] == 'pop':
+        if que:
+            print(que.popleft())
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
+        print(len(que))
+    elif cmd[0] == 'empty':
+        if len(que) == 0:
+            print(1)
+        else:
+            print(0)
+    elif cmd[0] == 'front':
+        if que:
+            print(que[0])
+        else:
+            print(-1)
+    else:
+        if que:
+            print(que[-1])
+        else:
+            print(-1)
+    n-=1
