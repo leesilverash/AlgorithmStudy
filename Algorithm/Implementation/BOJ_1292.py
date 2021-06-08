861ebcca2fc4ae8a1edf29f018e195864f6025d5
leesilverash <leesilverash@gmail.com>
2021-06-09 00:34:43 +0900

    Implementation-BOJ_1292.py

---
+++ Algorithm/Implementation/BOJ_1292.py
@@ -0,0 +1,13 @@
+import sys
+
+A, B = map(int, sys.stdin.readline().strip().split())
+answer = 0
+list = []
+i = 1
+
+while len(list) <= B:
+    list += [i]*i
+    i += 1
+
+answer = sum(list[A-1:B])
+print(answer)
