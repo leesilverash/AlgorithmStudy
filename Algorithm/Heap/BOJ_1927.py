50e4b302fedd686e7a25b3417862c97ddd9d9011
leesilverash <leesilverash@gmail.com>
2021-06-13 03:05:13 +0900

    Heap-BOJ_1927.py

--- Algorithm/Heap/BOJ_1927.py
+++ Algorithm/Heap/BOJ_1927.py
@@ -1,15 +1,14 @@
 import sys
 import heapq
-N= int(sys.stdin.readline().rstrip())
-
+N = int(sys.stdin.readline().rstrip())
 heap = []
-
-for i in range(N):
+for _ in range(N):
     x = int(sys.stdin.readline().rstrip())
     if x >= 1:
-        heapq.heappush(heap, (-x,x))
+        heapq.heappush(heap, x)
     else:
         if len(heap) == 0:
             print(0)
         else:
-            print(heapq.heappop(heap)[1])
+            print(heapq.heappop(heap))
+
