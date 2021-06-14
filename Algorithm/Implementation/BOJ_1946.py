340ffd3ea2f10df3d4481b66db88cb7d534e66f3
leesilverash <leesilverash@gmail.com>
2021-06-15 03:42:02 +0900

    Implementation-BOJ_1946.py

---
+++ Algorithm/Implementation/BOJ_1946.py
@@ -0,0 +1,22 @@
+import sys
+
+T = int(sys.stdin.readline().rstrip())
+
+while T > 0:
+    N = int(sys.stdin.readline().rstrip())
+    lst = [list(map(int, sys.stdin.readline().rstrip().split())) for i in range(N)]
+    lst.sort()
+    answer = 1
+    rank = lst[0][1]
+    for i in range(1, N):
+        if lst[i][1] < rank:
+            rank = lst[i][1]
+            answer += 1
+    print(answer)
+    T -= 1
+
+
+
+
+
+
