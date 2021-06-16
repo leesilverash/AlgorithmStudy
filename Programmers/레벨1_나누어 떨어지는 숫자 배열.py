449c07461f1d0887332a7303dae3e7f3a91afab5
leesilverash <leesilverash@gmail.com>
2021-06-16 22:40:41 +0900

    Programmers

---
+++ Programmers/레벨1_나누어 떨어지는 숫자 배열.py
@@ -0,0 +1,8 @@
+def solution(arr, divisor):
+    answer = []
+    for i in arr:
+        if i % divisor == 0:
+            answer.append(i)
+
+    answer.sort() if answer else answer.append(-1)
+    return answer
