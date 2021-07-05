37930252d45fb1bcbfe3dbc932dfc8eb1a5e6014
leesilverash <62590024+leesilverash@users.noreply.github.com>
2021-07-05 15:31:01 +0900

    Greedy-BOJ_1343.py

---
+++ BOJ/Greedy/BOJ_1343.py
@@ -0,0 +1,20 @@
+import sys
+
+s = sys.stdin.readline().rstrip()
+answer = ''
+lst = s.split('.')
+for i in range(len(lst)):
+    if len(lst[i]) % 4 == 0:
+        answer+='A'*len(lst[i])
+    elif len(lst[i]) % 2 == 0:
+        A = 4* (len(lst[i])//4)
+        B = len(lst[i])%4
+        answer+='A'*A
+        answer+='B'*B
+    else:
+        answer = -1
+        break
+    if i != len(lst)-1:
+        answer += '.'
+
+print(answer)
