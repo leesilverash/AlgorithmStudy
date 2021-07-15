c8d8505b06e37b415a69aff4aef31aaa6e9bd63e
leesilverash <leesilverash@gmail.com>
2021-07-15 23:02:11 +0900

    BOJ-Greedy_1181.py

---
+++ BOJ/Greedy/BOJ_1181.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+lst = []
+for _ in range(n):
+    s = sys.stdin.readline().rstrip()
+    cnt = len(s)
+    lst.append((s,cnt))
+lst = list(set(lst))
+lst.sort(key = lambda x: (x[1], x[0]))
+
+for i in lst:
+    print(i[0])
