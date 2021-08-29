cc7488de579dfd5c80e7d9d01bd10cec74d3edd0
leesilverash <leesilverash@gmail.com>
2021-08-29 19:44:44 +0900

    BOJ_3986.py edit

---
+++ Programmers/레벨1/레벨1_직업군 추천하기.py
@@ -0,0 +1,23 @@
+def solution(table, languages, preference):
+    answer = ''
+    score = {}
+    for i in range(len(languages)):
+        score[languages[i]] = preference[i]
+    max_score = 0
+    for information in table:
+        inf = information.split()
+        current_score = {inf[i]: 6 - i for i in range(1, len(inf))}
+        total_score = 0
+        for lan in score.keys():
+            if current_score.get(lan):
+                total_score += (current_score[lan] * score[lan])
+
+        if total_score == max_score:
+            if answer > inf[0]:
+                answer = inf[0]
+
+        if total_score > max_score:
+            max_score = total_score
+            answer = inf[0]
+
+    return answer
