50badfb1a07f5eceedc1d7a52290acfa0ceeb2d2
leesilverash <leesilverash@gmail.com>
2021-08-30 18:00:06 +0900

    BOJ_5555.py

---
+++ BOJ/BOJ_5555.py
@@ -0,0 +1,13 @@
+import sys
+
+str = sys.stdin.readline().rstrip()
+n = int(sys.stdin.readline().rstrip())
+lst = []
+count = 0
+for i in range(n):
+    lst.append(sys.stdin.readline().rstrip()*2)
+
+for i in range(len(lst)):
+    if str in lst[i]:
+        count += 1
+print(count)
