40e7734334ef6594115a02c5b1dc206c9e90bba0
leesilverash <leesilverash@gmail.com>
2021-11-14 23:11:52 +0900

    boj_1449

---
+++ 탐욕법/boj_1449_수리공항승.py
@@ -0,0 +1,15 @@
+n, l = map(int, input().split())
+coord = [False] * 1001
+for i in map(int, input().split()):
+    coord[i] = True
+
+ans = 0
+x = 0
+while x < 1001:
+    if coord[x]:
+        ans += 1
+        x += l
+    else:
+        x += 1
+
+print(ans)
