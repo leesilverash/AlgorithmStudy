ee05b60496dff423034a5478d4013213f47d9e63
leesilverash <leesilverash@gmail.com>
2022-01-16 16:21:23 +0900

    이분검색

---
+++ Algorithm&DataStructure/이분탐색(결정알고리즘)&그리디/이분검색.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+lst = list(map(int,input().split()))
+lst.sort()
+lt = 0
+rt = n-1
+while lt <= rt:
+    mid = (lt+rt)//2
+    if lst[mid] == m:
+        print(mid+1)
+        break
+    elif lst[mid] < m:
+        lt = mid+1
+    else:
+        rt = mid-1
+
