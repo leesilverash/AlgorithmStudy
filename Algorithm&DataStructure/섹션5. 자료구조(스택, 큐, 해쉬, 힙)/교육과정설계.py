9017a945e8b895dbe8e081cfdc8522793f5e5f9c
leesilverash <leesilverash@gmail.com>
2022-02-08 14:57:24 +0900

    교육과정설계

---
+++ Algorithm&DataStructure/섹션5. 자료구조(스택, 큐, 해쉬, 힙)/교육과정설계.py
@@ -0,0 +1,21 @@
+from collections import deque
+order = (input())
+num = int(input())
+lst = [input() for _ in range(num)]
+for i in range(len(lst)):
+    tmp = deque(order)
+    for j in lst[i]:
+        if tmp and j == tmp[0]:
+            tmp.popleft()
+
+    if len(tmp) > 0:
+        print("#{} No".format(i+1))
+    else:
+        print("#{} YES".format(i+1))
+
+
+# CBA
+# 3
+# CBDAGE
+# FGCDAB
+# CTSBDEA
