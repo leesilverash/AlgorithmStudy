a1d0bfd4f77334bb9c54e745086ce25f4bffdf40
leesilverash <leesilverash@gmail.com>
2022-02-28 12:26:12 +0900

    동전교환

--- Algorithm&DataStructure/섹션6. 완전탐색/동전교환.py
+++ Algorithm&DataStructure/섹션6. 완전탐색/동전교환.py
@@ -1,11 +1,23 @@
 n = int(input())
 coin = sorted(list(map(int, input().split())), reverse=True)
 m = int(input())
+ans = 214700000
 
 def DFS(l, sum):
+    global ans
+    if l >= ans:
+        return
+    if sum > m:
+        return
+    if sum == m:
+        if l < ans:
+            ans = l
+    else:
+        for i in range(n):
+            DFS(l+1, sum + coin[i])
 
-
-DFS()
+DFS(0,0)
+print(ans)
 # 3
 # 1 2 5
 # 15
