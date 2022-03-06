6947db19b77e8fc7b27b7609319e2898206d4821
leesilverash <leesilverash@gmail.com>
2022-03-07 03:08:43 +0900

    순열구하기

---
+++ Algorithm&DataStructure/섹션6. 완전탐색/순열구하기.py
@@ -0,0 +1,20 @@
+def dfs(l):
+    if l == m:
+        global ans
+        for i in range(m):
+            print(res[i], end = ' ')
+        print()
+        ans += 1
+    else:
+        for i in range(1, n+1): # [0, 0, 0, 0]
+            if ch[i] == 0:
+                ch[i] = 1
+                res[l] = i  # res [1, 0]
+                dfs(l+1)
+                ch[i] = 0
+
+n, m = map(int, input().split())
+res = [0] * m
+ch = [0] * (n+1)
+ans = 0
+dfs(0)
