74ca6879239f2b635f0c2cba2e8144a7f6c61168
leesilverash <leesilverash@gmail.com>
2022-03-07 03:09:11 +0900

    합이같은부분집합

--- Algorithm&DataStructure/섹션6. 완전탐색/합이같은부분집합.py
+++ Algorithm&DataStructure/섹션6. 완전탐색/합이같은부분집합.py
@@ -2,20 +2,18 @@ import sys
 n = int(input())
 lst = list(map(int, input().split()))
 total = sum(lst)
-
-def DFS(L, sum):
+def dfs(l, sum):
     if sum > total // 2:
         return
-    if L == n:
-        if sum == (total-sum):
-            print("YES")
+    if l == n:
+        if sum == total - sum:
+            print('YES')
             sys.exit(0)
     else:
-        DFS(L+1, sum+lst[L])
-        DFS(L+1, sum)
+        dfs(l+1, sum+lst[l])
+        dfs(l+1, sum)
 
-DFS(0, 0)
-print("NO")
-
+dfs(0,0)
+print('NO')
 # 6
 # 1 3 5 6 7 10
