91ec49ff476f08e9f79bd4ee6a2fcd7f0c09cd4f
leesilverash <leesilverash@gmail.com>
2022-04-12 16:13:05 +0900

    수들의합

--- Algorithm&DataStructure/섹션3.탐색 & 시뮬레이션/수들의합.py
+++ Algorithm&DataStructure/섹션3.탐색 & 시뮬레이션/수들의합.py
@@ -1,19 +1,41 @@
-n, m = map(int, input().split())
-lst = list(map(int, input().split()))
-answer = 0
-for i in range(n):
-    sum = lst[i]
-    for j in range(i+1, n):
-        if sum < m:
-            sum += lst[j]
-            if sum == m:
-                answer += 1
-                break
+# n, m = map(int, input().split())
+# lst = list(map(int, input().split()))
+# answer = 0
+# for i in range(n):
+#     sum = lst[i]
+#     for j in range(i+1, n):
+#         if sum < m:
+#             sum += lst[j]
+#             if sum == m:
+#                 answer += 1
+#                 break
+#         else:
+#             if sum == m:
+#                 answer += 1
+#                 break
+#             break
+# print(answer)
+
+n, m=map(int, input().split())
+a=list(map(int, input().split()))
+lt=0
+rt=1
+tot=a[0]
+cnt=0
+while True:
+    if tot<m:
+        if rt<n:
+            tot+=a[rt]
+            rt+=1
         else:
-            if sum == m:
-                answer += 1
-                break
             break
-print(answer)
+    elif tot==m:
+        cnt+=1
+        tot-=a[lt]
+        lt+=1
+    else:
+        tot-=a[lt]
+        lt+=1
+print(cnt)
 
 
